d61b5692f4062bb91d822f59b2dfa61ea014a701
juan-chaparo <juanchv71@gmail.com>
2021-11-06 16:46:18 -0500

    pruev

---
+++ Python/PrimeraP.py
@@ -0,0 +1,16 @@
+x=5.1
+#print(x)
+x+=1
+#print(x)
+x, y, z = 0.1,0.2,3
+#print(str(x+y)+" "+str(z))
+#print(f'{x+y:.3f} es la 10 parte de: {z}')
+a=f'{x**y} es la 10 parte de: {z}'
+#print(a)
+#x = input("Digite su edad" + str())
+#print("Su edad es"+x)
+j="hola mundo"
+""" for i in j:
+    print(i) """
+if "i" in j:
+    print("o")
