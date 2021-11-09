c4dd79c865b8bd45770b142be8dc654f9282ab98
juan-chaparo <juanchv71@gmail.com>
2021-11-09 18:04:19 -0500

    C

---
+++ Python/Cuarta.py
@@ -0,0 +1,18 @@
+""" class cj:
+    print(5)
+    def jc():
+        print(1)
+        return 8
+    def lo():
+        print(2)
+
+ """
+
+class objeto:
+    def __init__(self,a,b):
+        self.a = a
+        self.b = b
+    def op(self):
+        print(self.a)
+u=objeto(1,2)
+u.op()
